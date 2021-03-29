06137d343d5b3ff06a83451363fc82368ec8588d
Leandro Correia <leandrocorreia@ua.pt>
2021-03-29 18:47:18 +0100

    Teste de Edição 1.0
    Teste de edição de código - Leandro

--- src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
+++ src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
@@ -15,7 +15,7 @@ public class LP_LeandroCorreia_RuiSilva {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bem-vindo à plataforma do Bingo");
     }
     
 }
