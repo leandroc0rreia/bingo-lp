c50460d23c6321dc05d3e6c290d93331e121fc59
RuiSilva25 <Rfradinho@ua.pt>
2021-03-30 15:59:15 +0100

    teste de edição 2
    hey

--- src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
+++ src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
@@ -15,7 +15,10 @@ public class LP_LeandroCorreia_RuiSilva {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         System.out.println("Bem-vindo à plataforma do Bingo");
+    
     }
+    
     
 }
