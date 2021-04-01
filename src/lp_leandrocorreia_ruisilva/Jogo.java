f8a8dfaf76a179fcc98775260051d74e932cd666
RuiSilva25 <Rfradinho@ua.pt>
2021-04-01 15:36:28 +0100

    Atualização v.1.5
    -Run test

--- src/lp_leandrocorreia_ruisilva/Jogo.java
+++ src/lp_leandrocorreia_ruisilva/Jogo.java
@@ -55,13 +55,27 @@ public class Jogo {
     
     public static void numExt(){
         //Números inseridos anteriormente
-        
-        Scanner sc = new Scanner(System.in);
+      Scanner scn = new Scanner(System.in);
         int tamanho;
-        
-        System.out.print("Números sorteados: \n");
-        
-        
-    }
 
-}
+        System.out.print("Insira o tamanho da tabela ");
+        tamanho = scn.nextInt();
+
+        int[][] duasPos = new int[tamanho][tamanho];
+
+        for (int a = 0; a < duasPos.length; a++){
+            for(int b = 0; b < duasPos.length; b++){
+                System.out.print("Insira um numero para a posição " + a + "x" + b + " da tabela ");
+                duasPos[a][b]= scn.nextInt();
+            }
+        }
+
+        for (int[] colunas : duasPos){
+            for (int i : colunas) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}                    
--- src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
+++ src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
@@ -1,26 +1,66 @@
-public class Main {
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lp_leandrocorreia_ruisilva;
+import java.util.Scanner;
+/**
+ *
+ * @author Leandro Correia
+ */
+public class LP_LeandroCorreia_RuiSilva {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int tamanho;
-
-        System.out.print("Insira o tamanho da tabela ");
-        tamanho = scn.nextInt();
-
-        int[][] duasPos = new int[tamanho][tamanho];
-
-        for (int a = 0; a < duasPos.length; a++){
-            for(int b = 0; b < duasPos.length; b++){
-                System.out.print("Insira um numero para a posição " + a + "x" + b + " da tabela ");
-                duasPos[a][b]= scn.nextInt();
+        Scanner sc = new Scanner(System.in);     
+        Limpar l = new Limpar();
+        Jogo j = new Jogo();
+        
+        l.limparConsola();
+        System.out.println("Bem-vindo à plataforma do Bingo\n");
+        
+        System.out.println("O Bingo é um jogo de azar divertido no qual todos podem participar.\nNele, usa-se um cartão de quinze posições em que os números são sorteados\naleatoriamente, se conseguir preencher todos os quadrados do jogo, GANHA!\n");
+        System.out.println("Regras:\n\t1. Visualize os números do seu cartão;\n\t2. Introduza o número sorteado exteriormente;\n\t3. Grite 'Bingo' se tiver o cartão completo;\n\t4. Boa sorte e divirta-se!\n");
+        
+        System.out.print("Pressiona ENTER para continuar...");
+        sc.nextLine();
+        
+        int select;
+        do {
+            Limpar.limparConsola();
+            
+            System.out.println("\t\n1. Novo Jogo\t\n2. Recorde\t\n0. Sair\t\n");
+            System.out.print("Escolhe uma opção: ");
+            
+            select = sc.nextInt();
+            
+            switch(select){
+                case 1:
+                    l.limparConsola();
+                    
+                    j.numExt();
+                    j.cartão();
+                    sc.nextInt();
+                break;
+                case 2:
+                    l.limparConsola();
+                break;
+                case 0:
+                    l.limparConsola();
+                    System.out.print("Fechando...");
+                    sc.nextLine();
+                break;
+                default:
+                    l.limparConsola();
+                break;
             }
-        }
-
-        for (int[] colunas : duasPos){
-            for (int i : colunas) {
-                System.out.print(i);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
+            
+        } while (select!=0);
+        sc.nextLine();
     }
+    
 }
