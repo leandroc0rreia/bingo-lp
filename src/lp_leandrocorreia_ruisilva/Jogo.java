02eaea194d78eecd02e20ccbf077e274cc7dc2a0
Leandro Correia <leandrocorreia@ua.pt>
2021-04-06 11:13:19 +0100

    Atualização v1.7.2
    Esta atualização contém:
    
    - Alguma organização do código

--- src/lp_leandrocorreia_ruisilva/Jogo.java
+++ src/lp_leandrocorreia_ruisilva/Jogo.java
@@ -4,89 +4,90 @@
  * and open the template in the editor.
  */
 package lp_leandrocorreia_ruisilva;
+
 import java.util.Random;
 import java.util.Scanner;
-import java.lang.reflect.Array;
+
 /**
  *
  * @author Leandro Correia
  */
 public class Jogo {
+    
+    public static void sorteador(int cart[][]) {
 
-    public static void sorteador(int cart[][]){
-        
         Random r = new Random();
-        
+        // Soteio de número pela matriz cartão 3x9
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 3; j++) {
-                do{
-                    if(i==0){
-                        cart[i][j] = r.nextInt(9-1)+1;
+                do {
+                    if (i == 0) {
+                        cart[i][j] = r.nextInt(9 - 1) + 1;
                     }
-                    if(i==1){
-                        cart[i][j] = r.nextInt(19-10)+10;
+                    if (i == 1) {
+                        cart[i][j] = r.nextInt(19 - 10) + 10;
                     }
-                    if(i==2){
-                        cart[i][j] = r.nextInt(29-20)+20;
+                    if (i == 2) {
+                        cart[i][j] = r.nextInt(29 - 20) + 20;
                     }
-                    if(i==3){
-                        cart[i][j] = r.nextInt(39-30)+30;
+                    if (i == 3) {
+                        cart[i][j] = r.nextInt(39 - 30) + 30;
                     }
-                    if(i==4){
-                        cart[i][j] = r.nextInt(49-40)+40;
+                    if (i == 4) {
+                        cart[i][j] = r.nextInt(49 - 40) + 40;
                     }
-                    if(i==5){
-                        cart[i][j] = r.nextInt(59-50)+50;
+                    if (i == 5) {
+                        cart[i][j] = r.nextInt(59 - 50) + 50;
                     }
-                    if(i==6){
-                        cart[i][j] = r.nextInt(69-60)+60;
+                    if (i == 6) {
+                        cart[i][j] = r.nextInt(69 - 60) + 60;
                     }
-                    if(i==7){
-                        cart[i][j] = r.nextInt(79-70)+70;
+                    if (i == 7) {
+                        cart[i][j] = r.nextInt(79 - 70) + 70;
                     }
-                    if(i==8){
-                        cart[i][j] = r.nextInt(90-80)+80;
+                    if (i == 8) {
+                        cart[i][j] = r.nextInt(90 - 80) + 80;
                     }
-                }while(cart!=cart);
+                } while (cart != cart);
             }
         }
-        
+
         // Sorteio de posições a serem apagadas por linha
-        int colunaalea=0;
+        int colunaalea = 0;
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j<4; j++) {
-                
+            for (int j = 0; j < 4; j++) {
+
                 colunaalea = r.nextInt(9);
                 do {
                     colunaalea = r.nextInt(9);
-                } while (cart[colunaalea][i]==0);
+                } while (cart[colunaalea][i] == 0);
                 cart[colunaalea][i] = 0;
-                
+
             }
         }
     }
-    
-    public static void cartao(){
+
+    public static void cartao() {
         //Gerador de cartão com os números 
         Scanner sc = new Scanner(System.in);
         int cartao[][] = new int[9][3];
         sorteador(cartao);
 
-        
-        
         int bingo = 1;
-        
+
         for (int i = 0; i < bingo; i++) {
-            
+
             System.out.println("╔═════════╦═════════╦═════════╦═════════╦═════════╦═════════╦═════════╦═════════╦═════════╗");
             System.out.println("║         ║         ║         ║         ║         ║         ║         ║         ║         ║");
             System.out.print("║");
             for (int j = 0; j < 9; j++) {
-                if(j==0 || cartao[j][0]==0){System.out.print(" ");}
-                if(cartao[j][0]==0){
+                if (j == 0 || cartao[j][0] == 0) {
+                    System.out.print(" ");
+                }
+                if (cartao[j][0] == 0) {
                     System.out.print("        ║");
-                }else{
-                    System.out.print("   "+ cartao[j][0]+"    ║");
+                } else {
+                    System.out.print("   " + cartao[j][0] + "    ║");
                 }
             }
             System.out.println("\n║         ║         ║         ║         ║         ║         ║         ║         ║         ║");
@@ -94,39 +95,43 @@ public class Jogo {
             System.out.println("║         ║         ║         ║         ║         ║         ║         ║         ║         ║");
             System.out.print("║");
             for (int j = 0; j < 9; j++) {
-                if(j==0 || cartao[j][1]==0){System.out.print(" ");}
-                if(cartao[j][1]==0){
+                if (j == 0 || cartao[j][1] == 0) {
+                    System.out.print(" ");
+                }
+                if (cartao[j][1] == 0) {
                     System.out.print("        ║");
-                }else{
-                    System.out.print("   "+ cartao[j][1]+"    ║");
+                } else {
+                    System.out.print("   " + cartao[j][1] + "    ║");
                 }
             }
-            
+
             System.out.println("\n║         ║         ║         ║         ║         ║         ║         ║         ║         ║");
             System.out.println("╠═════════╬═════════╬═════════╬═════════╬═════════╬═════════╬═════════╬═════════╬═════════╣");
             System.out.println("║         ║         ║         ║         ║         ║         ║         ║         ║         ║");
             System.out.print("║");
             for (int j = 0; j < 9; j++) {
-                if(j==0 || cartao[j][2]==0){System.out.print(" ");}
-                if(cartao[j][2]==0){
+                if (j == 0 || cartao[j][2] == 0) {
+                    System.out.print(" ");
+                }
+                if (cartao[j][2] == 0) {
                     System.out.print("        ║");
-                }else{
-                    System.out.print("   "+ cartao[j][2]+"    ║");
+                } else {
+                    System.out.print("   " + cartao[j][2] + "    ║");
                 }
             }
             System.out.println("\n║         ║         ║         ║         ║         ║         ║         ║         ║         ║");
             System.out.println("╚═════════╩═════════╩═════════╩═════════╩═════════╩═════════╩═════════╩═════════╩═════════╝");
             sc.nextLine();
         }
-        
+
     }
     
-    public static void numExt(){
+    public static void numExt() {
         //Números inseridos anteriormente
-        
+
         Scanner sc = new Scanner(System.in);
         int tamanho;
-        
+
         System.out.print("Números sorteados: \n");
     }
 }
--- src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
+++ src/lp_leandrocorreia_ruisilva/LP_LeandroCorreia_RuiSilva.java
@@ -5,10 +5,10 @@
  * and open the template in the editor.
  */
 package lp_leandrocorreia_ruisilva;
+
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 /**
  *
  * @author Leandro Correia
@@ -19,56 +19,56 @@ public class LP_LeandroCorreia_RuiSilva {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);     
+        Scanner sc = new Scanner(System.in);
         Limpar l = new Limpar();
         Jogo j = new Jogo();
-        
+
         l.limparConsola();
         System.out.println("Bem-vindo à plataforma do Bingo\n");
-        
+
         System.out.println("O Bingo é um jogo de azar divertido no qual todos podem participar.\nNele, usa-se um cartão de quinze posições em que os números são sorteados\naleatoriamente, se conseguir preencher todos os quadrados do jogo, GANHA!\n");
         System.out.println("Regras:\n\t1. Visualize os números do seu cartão;\n\t2. Introduza o número sorteado exteriormente;\n\t3. Grite 'Bingo' se tiver o cartão completo;\n\t4. Boa sorte e divirta-se!\n");
-        
+
         System.out.print("Pressiona ENTER para continuar...");
         sc.nextLine();
-        
+
         int select;
         do {
             Limpar.limparConsola();
-            
+
             System.out.println("\t\n1. Novo Jogo\t\n2. Recorde\t\n0. Sair\t\n");
             System.out.print("Escolhe uma opção: ");
-            
+
             select = sc.nextInt();
-            
-            switch(select){
+
+            switch (select) {
                 case 1:
                     l.limparConsola();
-                    
+
                     j.numExt();
                     j.cartao();
                     sc.nextLine();
-                break;
+                    break;
                 case 2:
                     l.limparConsola();
-                break;
+                    break;
                 case 0:
                     l.limparConsola();
                     System.out.print("Fechando...");
                     sc.nextLine();
-                break;
+                    break;
                 default:
                     l.limparConsola();
-                break;
+                    break;
             }
-            
-        } while (select!=0);
-        
+
+        } while (select != 0);
+
         try {
             TimeUnit.SECONDS.sleep(3); // Espera de 3 segundos até fechar o programa
         } catch (InterruptedException ex) {
             // Nada acontece
         }
     }
-    
+
 }
